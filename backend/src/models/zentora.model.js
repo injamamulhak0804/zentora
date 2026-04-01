a68b19b63182a0df26e33b96bfd6497c5ef6141d
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-04-01 21:18:53 +0530

    feat: add basic schema

---
+++ backend/src/models/zentora.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model("User", userSchema);
