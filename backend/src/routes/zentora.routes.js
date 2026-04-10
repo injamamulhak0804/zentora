dda1bca65dab4634a76ab8c59e2bc38eabcbb73d
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-04-10 22:20:10 +0530

    feat: add verify and userAuth route

--- backend/src/routes/zentora.routes.js
+++ backend/src/routes/zentora.routes.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   getZentora,
   createZentora,
+  saveCanvas,
 } from "../controllers/zentora.controller.js";
+import { verifyAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -11,5 +13,11 @@ router.post("/user/signin", getZentora);
 
 // POST
 router.post("/user/signup", createZentora);
+
+//GET
+router.get("/verify", verifyAuth);
+
+//POST
+router.post("/user/save", saveCanvas);
 
 export default router;
