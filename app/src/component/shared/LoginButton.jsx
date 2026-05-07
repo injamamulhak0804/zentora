ef5bd600e94adbc6602d1e1a5a4c845ee23c8ce4
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-05-07 21:22:26 +0530

    feat: refactor LoginButton component to accept props and enhance UI

--- app/src/component/shared/LoginButton.jsx
+++ app/src/component/shared/LoginButton.jsx
@@ -1,14 +1,19 @@
-import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
+import { FcGoogle } from "react-icons/fc";
 
-function LoginButton() {
+function LoginButton({
+  navigate,
+  buttonText = "Login with Google",
+  onError,
+  setUserData,
+}) {
   const login = useGoogleLogin({
     onSuccess: async (response) => {
-      console.log(response);
-      // You can send the token to your backend for further processing
       const res = await fetch(
         import.meta.env.VITE_BACKEND_URL + "/api/v1/auth/google",
         {
           method: "POST",
+          credentials: "include",
           headers: {
             "Content-Type": "application/json",
           },
@@ -19,15 +24,30 @@ function LoginButton() {
       );
 
       const data = await res.json();
+      console.log("data: ", data);
 
-      console.log(data);
+      if (data.success) {
+        localStorage.setItem("token", data.token);
+        setUserData(data.data);
+        navigate("/");
+      } else {
+        onError?.(data.message || "Something went wrong");
+      }
     },
     onError: () => {
-      console.log("Login Failed");
+      onError?.("Google login failed. Please try again.");
     },
   });
 
-  return <button onClick={() => login()}>Login with Google</button>;
+  return (
+    <button
+      className="mt-3 w-full cursor-pointer rounded-md border-2 border-gray-300 py-2 text-gray-700 hover:bg-gray-50 flex items-center justify-center gap-2"
+      onClick={() => login()}
+    >
+      <FcGoogle size={20} />
+      {buttonText}
+    </button>
+  );
 }
 
 export default LoginButton;
